90048d9cb16141bb770407a6ddb00e4548d81523
kihak12 <jeremyperret43@gmail.com>
2024-03-21 10:39:54 +0100

    first commit

---
+++ scripts/pages/index.js
@@ -0,0 +1,22 @@
+async function getPhotographers() {
+    return await fetch('./data/photographers.json').then(response => response.json());
+}
+
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerTemplate(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+}
+
+async function init() {
+    // Récupère les datas des photographes
+    const {photographers} = await getPhotographers();
+    displayData(photographers);
+}
+
+init();
+    
