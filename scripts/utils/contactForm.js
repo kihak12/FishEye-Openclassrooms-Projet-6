479e76d4165d047b8547bddc2c343cd40c078e55
kihak12 <jeremyperret43@gmail.com>
2024-05-03 11:13:58 +0200

    log value filed in contact form

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,4 +1,5 @@
 const modal = document.getElementById("contact_modal");
+const submitForm = document.getElementById("contact-form");
 
 function displayContactModal() {
     document.getElementById('contact-photographer-name').innerText= photographerDetails.photographer.name;
@@ -8,3 +9,21 @@ function displayContactModal() {
 function closeContactModal() {
     modal.close();
 }
+
+submitForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const prenom = document.getElementById('prenom').value;
+    const nom = document.getElementById('nom').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    console.log('contact data', {
+            "prenom": prenom,
+            "nom": nom,
+            "email": email,
+            "message": message
+        });
+
+    modal.close();
+})
